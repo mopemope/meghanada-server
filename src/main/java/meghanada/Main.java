3d731f1c08016164c969564146ab29e20460a6e4
mopemope <yutaka.matsubara@gmail.com>
2016-09-27 07:58:57 +0900

    Started 0.2.2

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.2.2";
 
     private static Logger log = LogManager.getLogger(Main.class);
 
