c9b5c28434a1772bfb524a1fe6d727bff8bcc7bd
mopemope <yutaka.matsubara@gmail.com>
2016-10-06 23:42:25 +0900

    Start 0.2.4

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "0.2.3";
+    public static final String VERSION = "0.2.4";
 
     private static Logger log = LogManager.getLogger(Main.class);
 
