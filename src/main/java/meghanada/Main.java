41efd9dadbf23a338b8d9d162dc7daf9cb322aa1
mopemope <yutaka.matsubara@gmail.com>
2016-10-19 08:36:44 +0900

    Update version

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "0.2.4";
+    public static final String VERSION = "0.2.5";
 
     private static Logger log = LogManager.getLogger(Main.class);
 
