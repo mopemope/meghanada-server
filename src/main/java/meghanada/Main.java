a2f9a708df5dce594b9cb713144ddaf722f1dcfa
mopemope <yutaka.matsubara@gmail.com>
2017-01-23 08:32:32 +0900

    Fix version

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "0.3.0";
+    public static final String VERSION = "0.3.1";
 
     private static Logger log = LogManager.getLogger(Main.class);
 
