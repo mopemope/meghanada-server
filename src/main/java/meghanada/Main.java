719a23b57e7b8227d46c571d982acdc83ee3ea4d
mopemope <yutaka.matsubara@gmail.com>
2017-02-21 08:35:40 +0900

    Bump up

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static final String VERSION = "0.6.2";
+    public static final String VERSION = "0.6.3";
 
     private static Logger log = LogManager.getLogger(Main.class);
 
