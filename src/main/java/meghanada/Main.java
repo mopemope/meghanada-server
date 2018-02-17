8d297abf7c11f0e7f52f6be30b5c278c6bbcf3f8
mopemope <yutaka.matsubara@gmail.com>
2018-02-17 16:52:06 +0900

    Bump up

--- src/main/java/meghanada/Main.java
+++ src/main/java/meghanada/Main.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class Main {
 
-  public static final String VERSION = "0.9.1";
+  public static final String VERSION = "0.9.2";
 
   private static final Logger log = LogManager.getLogger(Main.class);
   private static String version;
