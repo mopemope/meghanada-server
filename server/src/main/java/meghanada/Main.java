64cd275886bc284f427d82316b8aacc8eba4b794
mopemope <yutaka.matsubara@gmail.com>
2018-04-04 21:42:44 +0900

    Bump up

--- server/src/main/java/meghanada/Main.java
+++ server/src/main/java/meghanada/Main.java
@@ -25,7 +25,7 @@ import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class Main {
 
-  public static final String VERSION = "0.9.3";
+  public static final String VERSION = "1.0.0";
 
   private static final Logger log = LogManager.getLogger(Main.class);
   private static String version;
