53c64cc496df63ba861517e86fcc20c11859f20f
Liu Xiang Qian <xqliu@icloud.com>
2018-05-16 02:25:16 +0800

    Changes as below:
    1. Add config camel-case-completion: whether to use camel case completion
    2. Cache the pattern and match result to boost complete speed.

--- server/src/main/java/meghanada/utils/StringUtils.java
+++ server/src/main/java/meghanada/utils/StringUtils.java
@@ -1,60 +1,74 @@
 package meghanada.utils;
 
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
-import java.lang.ref.SoftReference;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.HashMap;
-
 import com.google.common.base.Splitter;
+import meghanada.config.Config;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static java.util.Objects.isNull;
 
 public class StringUtils {
+
+  private static final String PatternForLowerCaseWord = "[^A-Z]*";
+  private static final String PatternForAnything = ".*";
+  private static final String PatternForSplitByUpper = "(?=\\p{Upper})";
   private static final StringUtils instance = new StringUtils();
   private static final Map<String, Pattern> cachedPatterns = new HashMap<>(500);
+  private static final Map<Integer, Boolean> cachedResult = new HashMap<>(500);
+
+  private boolean useccc;
+
+  void setUseccc(boolean useccc) {
+    this.useccc = useccc;
+  }
 
   public static StringUtils getInstance() {
     return instance;
   }
 
   private StringUtils() {
+    try {
+      setUseccc(Config.load().useCamelCaseCompletion());
+    } catch (Exception e) {
+      setUseccc(true);
+    }
   }
 
   public boolean isMatch(String name, String target) {
     boolean matched = false;
     if (org.apache.commons.lang3.StringUtils.isEmpty(target)) {
       matched = true;
+    } else if (!useccc) {
+      matched = name.toLowerCase().contains(target.toLowerCase());
     } else {
-      if (hasUpperCase(target)) {
-        Pattern p = cachedPatterns.get(target);
-        if (isNull(p)) {
-          List<String> r = Splitter
-            .onPattern("(?=\\p{Upper})")
-            .splitToList(target);
-          StringBuilder sb = new StringBuilder();
-          if (r.size() > 0) {
-            for (String s : r) {
-              sb.append(s).append("[^A-Z]*");
-            }
-            sb.append(".*");
-            p = Pattern.compile(sb.toString());
-            cachedPatterns.put(target, p);
-          }
-        }
-        if (isNull(p)) {
-          matched = name.contains(target);
-        } else {
-          Matcher m = p.matcher(name);
-          matched = m.matches();
-        }
+      Integer key = name.hashCode()/2 + target.hashCode()/2;
+      Boolean result = cachedResult.get(key);
+      if (!isNull(result)) {
+        matched = result;
       } else {
-        matched = matchWhenAllLowerCaseLetters(name, target);
+        if (hasUpperCase(target)) {
+          Pattern pattern = fromCacheOrCompile(target);
+          if (isNull(pattern)) {
+            matched = name.contains(target);
+          } else {
+            matched = matchAndUpdateCache(pattern, name, key);
+          }
+        } else {
+          matched = matchWhenAllLowerCaseLetters(name, target);
+        }
       }
     }
+    return matched;
+  }
+
+  private Boolean matchAndUpdateCache(Pattern p, String name, Integer key) {
+    Matcher m = p.matcher(name);
+    boolean matched = m.matches();
+    cachedResult.put(key, matched);
     return matched;
   }
 
@@ -64,5 +78,30 @@ public class StringUtils {
 
   private boolean matchWhenAllLowerCaseLetters(String name, String target) {
     return name.startsWith(target);
+  }
+
+  private Pattern compilePattern(List<String> input) {
+    Pattern p = null;
+    if (input.size() > 0) {
+      StringBuilder sb = new StringBuilder();
+      for (String s : input) {
+        sb.append(s).append(PatternForLowerCaseWord);
+      }
+      sb.append(PatternForAnything);
+      p = Pattern.compile(sb.toString());
+    }
+    return p;
+  }
+
+  private Pattern fromCacheOrCompile(String target) {
+    Pattern pattern = cachedPatterns.get(target);
+    if (isNull(pattern)) {
+      List<String> r = Splitter.onPattern(PatternForSplitByUpper).splitToList(target);
+      pattern = compilePattern(r);
+      if (!isNull(pattern)) {
+        cachedPatterns.put(target, pattern);
+      }
+    }
+    return pattern;
   }
 }
