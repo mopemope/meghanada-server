9316dacc4355370ad987e884bb84dfd9acf569b8
mopemope <yutaka.matsubara@gmail.com>
2019-02-25 22:46:07 +0900

    update plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("com.gradle.build-scan").version("2.0.2")
+    id("com.gradle.build-scan").version("2.1")
     id("com.github.ben-manes.versions").version("0.20.0")
     id("org.ajoberstar.grgit").version("3.0.0")
     id("com.github.sherter.google-java-format").version("0.8")
-    id("net.ltgt.errorprone").version("0.6.1")
+    id("net.ltgt.errorprone").version("0.7.1")
 }
 
 repositories {
