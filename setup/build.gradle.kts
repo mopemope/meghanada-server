523294ed7fffb547dcd7ff5434c283367e0f7513
mopemope <yutaka.matsubara@gmail.com>
2019-02-25 18:53:07 +0900

    replace to build.gradle.kts

---
+++ setup/build.gradle.kts
@@ -0,0 +1,108 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import com.jfrog.bintray.gradle.BintrayExtension
+import com.jfrog.bintray.gradle.tasks.RecordingCopyTask
+import org.ajoberstar.grgit.Grgit
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+plugins {
+    id("java")
+    id("maven")
+    id("application")
+    id("com.github.johnrengelman.shadow").version("4.0.4")
+}
+
+val group = "meghanada"
+val setupVersion = "0.0.2"
+var buildVersion = "release"
+
+val gitFile = File("./.git")
+if (gitFile.exists()) {
+    val grgit = Grgit.open()
+    buildVersion = grgit.head().abbreviatedId
+}
+val longVersion = "$setupVersion-$buildVersion"
+val date: String = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"))
+val applicationName = "meghanada-setup"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation("commons-cli:commons-cli:1.4")
+}
+
+application {
+    mainClassName = "meghanada.SetupMain"
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_KEY")
+
+    publish = true
+
+    filesSpec(delegateClosureOf<RecordingCopyTask> {
+        from("build/libs")
+        into(".")
+    })
+
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "meghanada"
+        name = "meghanada-setup"
+        vcsUrl = "https://github.com/mopemope/meghanada-server.git"
+        githubRepo = "mopemope/meghanada-server"
+        githubReleaseNotesFile = "README.md"
+        setLicenses("GPL-3.0")
+        setLabels("java", "emacs")
+
+        version(delegateClosureOf<BintrayExtension.VersionConfig> {
+            name = "$setupVersion"
+            desc = "Meghanada Server setup $setupVersion"
+            released = LocalDateTime.now().toString()
+        })
+
+    })
+}
+
+tasks {
+
+    val processResources by existing
+    val classes by existing
+    val shadowJar by existing
+    val clean by existing
+
+    withType<ShadowJar> {
+        baseName = "meghanada-setup"
+        classifier = null
+        version = setupVersion
+    }
+
+    val embedVersion = register<Copy>("embedVersion") {
+        from("src/main/resources/VERSION")
+        into("build/resources/main")
+        expand(Pair("buildDate", date), Pair("version", longVersion), Pair("appName", applicationName))
+        dependsOn(processResources)
+    }
+
+    classes {
+        dependsOn(embedVersion)
+    }
+
+    val installEmacsHome = register<Copy>("installEmacsHome") {
+        val home = System.getProperty("user.home")
+        from("build/libs/meghanada-setup-${setupVersion}.jar")
+        into("$home/.emacs.d/meghanada/")
+        dependsOn(shadowJar)
+    }
+
+    clean {
+        doLast({
+            file(".meghanada").deleteRecursively()
+        })
+    }
+}
+
