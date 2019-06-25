ef2053a9ad11f8c09b0dbb344cbcebeb18e46a39
mopemope <yutaka.matsubara@gmail.com>
2019-06-26 07:31:36 +0900

    update libs

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("org.apache.maven:maven-model-builder:3.6.1")
     implementation("com.leacox.motif:motif:0.1")
     implementation("com.leacox.motif:motif-hamcrest:0.1")
-    implementation("com.github.javaparser:javaparser-core:3.14.2")
+    implementation("com.github.javaparser:javaparser-core:3.14.5")
     implementation("org.apache.logging.log4j:log4j-core:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-api:$log4jVersion")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:$log4jVersion")
@@ -97,7 +97,7 @@ dependencies {
     implementation("io.opencensus:opencensus-exporter-trace-stackdriver:$opencensusVersion")
     implementation("io.opencensus:opencensus-exporter-stats-stackdriver:$opencensusVersion")
 
-    implementation("com.github.oshi:oshi-core:3.13.2")
+    implementation("com.github.oshi:oshi-core:3.13.3")
 }
 
 application {
