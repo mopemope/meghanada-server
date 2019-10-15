0c511e4016d9458ecd30965a86993de59199bc98
mopemope <yutaka.matsubara@gmail.com>
2019-10-16 00:10:25 +0900

    fix format

--- server/src/main/java/meghanada/server/formatter/SExprOutputFormatter.java
+++ server/src/main/java/meghanada/server/formatter/SExprOutputFormatter.java
@@ -187,7 +187,7 @@ public class SExprOutputFormatter implements OutputFormatter {
                     default:
                       break;
                   }
-                  
+
                   return LPAREN
                       + String.join(
                           LIST_SEP,
